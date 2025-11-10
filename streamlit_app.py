9fc46baa51aac0f04b7456426db910276389d5d4
6658065556PS <159753272+6658065556PS@users.noreply.github.com>
2025-11-10 14:31:49 +0700

    c3po

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,8 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Hello I am C3PO, your personal assistant"
+    "You know better than to trust a strange computer, I am a friend"
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -32,7 +31,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("Exciting"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
